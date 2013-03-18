6f1f931d4392f98246cf04c9ad0626cf13e9fb01
lutaol <lutaol@sfu.ca>
2013-03-18 01:51:48 -0700

    Add SSL in production

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 DemoApp::Application.initialize!
+
+DemoApp::Application.configure do
+  # Force all access to the app over SSL, use Strict-Transport-Security, 
+  # and use secure cookies.
+  config.force_ssl = true
+end
